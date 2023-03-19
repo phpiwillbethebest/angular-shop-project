0f7f0033b28aa643df1505d0ac10968794892db3
Victor <overhandvictor@gmail.com>
2023-03-19 20:52:13 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./components/pages/home/home.component";
+import {ShopComponent} from "./components/pages/shop/shop.component";
+import {AboutUsComponent} from "./components/pages/about-us/about-us.component";
+import {ContactComponent} from "./components/pages/contact/contact.component";
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'shop', component: ShopComponent},
+  {path: 'aboutus', component: AboutUsComponent},
+  {path: 'contact', component: ContactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { ShopComponent } from './components/pages/shop/shop.component';
+import { AboutUsComponent } from './components/pages/about-us/about-us.component';
+import { ContactComponent } from './components/pages/contact/contact.component';
+import { HeroComponent } from './components/pages/home/hero/hero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ShopComponent,
+    AboutUsComponent,
+    ContactComponent,
+    HeroComponent,
   ],
   imports: [
     BrowserModule,
